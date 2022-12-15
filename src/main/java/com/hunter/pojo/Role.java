a009d49b1ada31f869f82c6717a3640fec39c655
Hunter <hspecial@163.com>
2022-12-15 23:29:29 +0800

    编写实体类

---
+++ src/main/java/com/hunter/pojo/Role.java
@@ -0,0 +1,94 @@
+package com.hunter.pojo;
+
+
+import java.util.Date;
+
+public class Role {
+    private Integer id;
+
+    /**
+     * 角色编码
+     */
+    private String roleCode;
+
+    /**
+     * 角色名称
+     */
+    private String roleName;
+
+    /**
+     * 创建者
+     */
+    private Integer createdBy;
+
+    /**
+     * 创建时间
+     */
+    private java.sql.Timestamp creationDate;  // 创建时间
+
+    /**
+     * 更新者
+     */
+    private Integer modifyBy;
+
+    /**
+     * 更新时间
+     */
+    private Date modifyDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public java.sql.Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(java.sql.Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(Integer modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
---
+++ src/main/java/com/hunter/pojo/User.java
@@ -0,0 +1,179 @@
+package com.hunter.pojo;
+
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class User {
+    private Integer id;
+
+    /**
+     * 用户编码
+     */
+    private String userCode;
+
+    /**
+     * 用户名称
+     */
+    private String userName;
+
+    private String userPassword;
+    private Integer gender;
+    private Date birthday;
+
+    private String phone;
+    private String address;
+
+    /**
+     * 用户角色
+     */
+    private Integer userRole;
+
+    /**
+     * 创建者
+     */
+    private Integer createdBy;
+
+    /**
+     * 创建时间
+     */
+    private Date creationDate;
+
+    /**
+     * 更新者
+     */
+    private Integer modifyBy;
+
+    /**
+     * 更新时间
+     */
+    private Date modifyDate;
+
+    /**
+     * 用户角色名称
+     */
+    private String userRoleName;
+
+    public String getUserRoleName() {
+        return userRoleName;
+    }
+
+    public void setUserRoleName(String userRoleName) {
+        this.userRoleName = userRoleName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Integer userRole) {
+        this.userRole = userRole;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(Integer modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public Integer getAge() {
+        Date date = new Date();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        int currentYear = calendar.get(Calendar.YEAR);
+        calendar.setTime(birthday);
+        int birthYear = calendar.get(Calendar.YEAR);
+        return currentYear - birthYear;
+    }
+}
