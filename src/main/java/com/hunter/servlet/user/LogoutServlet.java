bb16e4241480a85f1a2223e04889e557eac0b1e5
Hunter <hspecial@163.com>
2022-12-18 17:18:42 +0800

    实现退出登录功能

---
+++ src/main/java/com/hunter/servlet/user/LogoutServlet.java
@@ -0,0 +1,24 @@
+package com.hunter.servlet.user;
+
+import com.hunter.util.Constants;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 移除用户的session
+        req.getSession().removeAttribute(Constants.USER_SESSION);
+        // 返回登录页面，填写绝对路径
+        resp.sendRedirect(req.getContextPath() + "/login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
